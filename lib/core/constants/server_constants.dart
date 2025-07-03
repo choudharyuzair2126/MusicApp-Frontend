f6578be1245ee9bb88458c582c4057b4e834cd96
choudhary2126 <uzair2126@proton.me>
2025-07-03 13:41:14 +0500

    Updated

--- lib/core/constants/server_constants.dart
+++ lib/core/constants/server_constants.dart
@@ -1,4 +1,4 @@
 class ServerConstants {
   static const String serverUrl =
-      'https://bb12c26c-ab99-4182-af75-f7351ea8072d-00-2bn1h4nbw3j50.sisko.replit.dev';
+      'Put your server URL here'; // Replace with your actual server URL
 }
